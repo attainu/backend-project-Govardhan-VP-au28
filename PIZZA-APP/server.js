e61601be78f7f49e68073e787b9310e6cb1accb9
Govardhan <govardhanvp23@gmail.com>
2022-07-22 12:04:30 +0530

    initial

---
+++ PIZZA-APP/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const path = require('path');
+const expressLayout = require('express-ejs-layouts');
+const PORT = process.env.PORT || 2022
+
+
+// routes
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+// set templates
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'));
+app.set('view engine', 'ejs');
+
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+})
