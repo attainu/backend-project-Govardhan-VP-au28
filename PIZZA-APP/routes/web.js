15f9733127fbc8ce75dbeec074a1b1427738f35f
Govardhan <govardhanvp23@gmail.com>
2022-07-30 20:47:10 -0400

    socket integration

--- PIZZA-APP/routes/web.js
+++ PIZZA-APP/routes/web.js
@@ -1,49 +1,9 @@
-// const authController = require('../app/http/controllers/authController');
-// const cartController = require('../app/http/controllers/customers/cartController');
-// const homeController = require('../app/http/controllers/homeController')
-// const guest = require('../app/http/middleware/guest');
-
-// function initRoutes(app) {
-
-
-//     app.get('/', homeController().index)
-
-//     // (req, res) => {
-//     //     res.render('home');
-//     // }
-
-//     app.get('/cart', cartController().index);
-
-
-//     app.get('/login', guest, authController().login)
-//     app.post('/login', authController().postLogin)
-
-//     app.get('/register', guest, authController().register)
-//     app.post('/register', authController().postRegister)
-//     app.post('/logout', authController().logout)
-
-
-//     app.post('/update-cart', cartController().update)
-
-//     // Customer routes
-//     // app.post('/orders', auth, orderController().store)
-//     // app.get('/customer/orders', auth, orderController().index)
-//     // app.get('/customer/orders/:id', auth, orderController().show)
-
-//     // // Admin routes
-//     // app.get('/admin/orders', admin, adminOrderController().index)
-//     // app.post('/admin/order/status', admin, statusController().update)
-
-// }
-
-// module.exports = initRoutes
-
 const homeController = require('../app/http/controllers/homeController')
 const authController = require('../app/http/controllers/authController')
 const cartController = require('../app/http/controllers/customers/cartController')
 const orderController = require('../app/http/controllers/customers/orderController')
-const adminOrderController = require('../app/http/controllers/admin/adminOrderController')
-    // const statusController = require('../app/http/controllers/admin/statusController')
+const adminOrderController = require('../app/http/controllers/admin/orderController')
+const statusController = require('../app/http/controllers/admin/statusController')
 
 // Middlewares 
 const guest = require('../app/http/middleware/guest')
@@ -64,11 +24,11 @@ function initRoutes(app) {
     // Customer routes
     app.post('/orders', auth, orderController().store)
     app.get('/customer/orders', auth, orderController().index)
-        // app.get('/customer/orders/:id', auth, orderController().show)
+    app.get('/customer/orders/:id', auth, orderController().show)
 
     // Admin routes
     app.get('/admin/orders', admin, adminOrderController().index)
-        // app.post('/admin/order/status', admin, statusController().update)
+    app.post('/admin/order/status', admin, statusController().update)
 }
 
 module.exports = initRoutes
--- PIZZA-APP/server.js
+++ PIZZA-APP/server.js
@@ -10,6 +10,7 @@ const session = require('express-session');
 const flash = require('express-flash');
 const MongoDbStore = require('connect-mongo');
 const passport = require('passport');
+const Emitter = require('events');
 
 const PORT = process.env.PORT || 2022
 
@@ -33,7 +34,9 @@ async function initMongoDB() {
 initMongoDB()
 
 
-
+// Event emitter
+const eventEmitter = new Emitter()
+app.set('eventEmitter', eventEmitter)
 
 //session-config--
 app.use(session({
@@ -82,6 +85,24 @@ require('./routes/web')(app);
 
 
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
+})
+
+// Socket
+
+const io = require('socket.io')(server)
+io.on('connection', (socket) => {
+    // Join
+    socket.on('join', (orderId) => {
+        socket.join(orderId)
+    })
+})
+
+eventEmitter.on('orderUpdated', (data) => {
+    io.to(`order_${data.id}`).emit('orderUpdated', data)
+})
+
+eventEmitter.on('orderPlaced', (data) => {
+    io.to('adminRoom').emit('orderPlaced', data)
 })
